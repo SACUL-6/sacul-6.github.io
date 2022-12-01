cd70ade159166fc340b458e556dfe2e6cba5f3b1
Glitch (glitch-blank-website) <none>
2022-12-01 18:43:23 +0000

    💘✋ Checkpoint ./G/0.0009765625/js/local_score_manager.js:25400050/5063 ./G/1/js/local_score_manager.js:25400050/6 ./G/0.0009765625/index.html:25400050/274

--- G/0.0009765625/js/local_score_manager.js
+++ G/0.0009765625/js/local_score_manager.js
@@ -1,130 +1,48 @@
-function KeyboardInputManager() {
-    this.events = {};
+window.fakeStorage = {
+    _data: {},
   
-    if (window.navigator.msPointerEnabled) {
-      //Internet Explorer 10 style
-      this.eventTouchstart    = "MSPointerDown";
-      this.eventTouchmove     = "MSPointerMove";
-      this.eventTouchend      = "MSPointerUp";
-    } else {
-      this.eventTouchstart    = "touchstart";
-      this.eventTouchmove     = "touchmove";
-      this.eventTouchend      = "touchend";
+    setItem: function (id, val) {
+      return this._data[id] = String(val);
+    },
+  
+    getItem: function (id) {
+      return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+    },
+  
+    removeItem: function (id) {
+      return delete this._data[id];
+    },
+  
+    clear: function () {
+      return this._data = {};
     }
+  };
   
-    this.listen();
+  function LocalScoreManager() {
+    this.key     = "bestScore | 0.0009765625";
+  
+    var supported = this.localStorageSupported();
+    this.storage = supported ? window.localStorage : window.fakeStorage;
   }
   
-  KeyboardInputManager.prototype.on = function (event, callback) {
-    if (!this.events[event]) {
-      this.events[event] = [];
-    }
-    this.events[event].push(callback);
-  };
+  LocalScoreManager.prototype.localStorageSupported = function () {
+    var testKey = "test";
+    var storage = window.localStorage;
   
-  KeyboardInputManager.prototype.emit = function (event, data) {
-    var callbacks = this.events[event];
-    if (callbacks) {
-      callbacks.forEach(function (callback) {
-        callback(data);
-      });
+    try {
+      storage.setItem(testKey, "1");
+      storage.removeItem(testKey);
+      return true;
+    } catch (error) {
+      return false;
     }
   };
   
-  KeyboardInputManager.prototype.listen = function () {
-    var self = this;
-  
-    var map = {
-      38: 0, // Up
-      39: 1, // Right
-      40: 2, // Down
-      37: 3, // Left
-      75: 0, // vim keybindings
-      76: 1,
-      74: 2,
-      72: 3,
-      87: 0, // W
-      68: 1, // D
-      83: 2, // S
-      65: 3  // A
-    };
-  
-    document.addEventListener("keydown", function (event) {
-      var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
-                      event.shiftKey;
-      var mapped    = map[event.which];
-  
-      if (!modifiers) {
-        if (mapped !== undefined) {
-          event.preventDefault();
-          self.emit("move", mapped);
-        }
-  
-        if (event.which === 32) self.restart.bind(self)(event);
-      }
-    });
-  
-    var retry = document.querySelector(".retry-button");
-    retry.addEventListener("click", this.restart.bind(this));
-    retry.addEventListener(this.eventTouchend, this.restart.bind(this));
-  
-    var keepPlaying = document.querySelector(".keep-playing-button");
-    keepPlaying.addEventListener("click", this.keepPlaying.bind(this));
-    keepPlaying.addEventListener("touchend", this.keepPlaying.bind(this));
-  
-    // Listen to swipe events
-    var touchStartClientX, touchStartClientY;
-    var gameContainer = document.getElementsByClassName("game-container")[0];
-  
-    gameContainer.addEventListener(this.eventTouchstart, function (event) {
-      if (( !window.navigator.msPointerEnabled && event.touches.length > 1) || event.targetTouches > 1) return;
-      
-      if(window.navigator.msPointerEnabled){
-          touchStartClientX = event.pageX;
-          touchStartClientY = event.pageY;
-      } else {
-          touchStartClientX = event.touches[0].clientX;
-          touchStartClientY = event.touches[0].clientY;
-      }
-      
-      event.preventDefault();
-    });
-  
-    gameContainer.addEventListener(this.eventTouchmove, function (event) {
-      event.preventDefault();
-    });
-  
-    gameContainer.addEventListener(this.eventTouchend, function (event) {
-      if (( !window.navigator.msPointerEnabled && event.touches.length > 0) || event.targetTouches > 0) return;
-  
-      var touchEndClientX, touchEndClientY;
-      if(window.navigator.msPointerEnabled){
-          touchEndClientX = event.pageX;
-          touchEndClientY = event.pageY;
-      } else {
-          touchEndClientX = event.changedTouches[0].clientX;
-          touchEndClientY = event.changedTouches[0].clientY;
-      }
-  
-      var dx = touchEndClientX - touchStartClientX;
-      var absDx = Math.abs(dx);
-  
-      var dy = touchEndClientY - touchStartClientY;
-      var absDy = Math.abs(dy);
-  
-      if (Math.max(absDx, absDy) > 10) {
-        // (right : left) : (down : up)
-        self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
-      }
-    });
+  LocalScoreManager.prototype.get = function () {
+    return this.storage.getItem(this.key) || 0;
   };
   
-  KeyboardInputManager.prototype.restart = function (event) {
-    event.preventDefault();
-    this.emit("restart");
+  LocalScoreManager.prototype.set = function (score) {
+    this.storage.setItem(this.key, score);
   };
   
-  KeyboardInputManager.prototype.keepPlaying = function (event) {
-    event.preventDefault();
-    this.emit("keepPlaying");
-  };  
--- G/1/js/local_score_manager.js
+++ G/1/js/local_score_manager.js
@@ -19,7 +19,7 @@ window.fakeStorage = {
   };
   
   function LocalScoreManager() {
-    this.key     = "bestScore";
+    this.key     = "bestScore | 1";
   
     var supported = this.localStorageSupported();
     this.storage = supported ? window.localStorage : window.fakeStorage;
