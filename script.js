f8687e31aa293376a24f4e124a06003e5eb6f660
Glitch (glitch-blank-website) <none>
2022-12-02 01:43:52 +0000

    🍦💒 Checkpoint ./script.js:25400050/143 ./index.html:25400050/64

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 /* Nothing here*/
 
 console.log("Hello! Welcome to my site!");
-con
+console.log("reminder to not paste anything into here on a site where you have to log in unless you know what you're doing")
